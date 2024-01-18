c35700dec0dce6d6254064fe557596573ec2e73f
Lunarducks <157049035+Lunarducks@users.noreply.github.com>
2024-01-19 01:00:47 +0530

    Update composer-install.sh

--- Scripts/composer-install.sh
+++ Scripts/composer-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/www/app.lunarducks.com/
-chmod -R 777 /var/www/app.lunarducks.com
-chown -R www-data:www-data /var/www/app.lunarducks.com
+chmod -R 777 /var/www/app.lunarducks.com/
+chown -R www-data:www-data /var/www/app.lunarducks.com/
 
 
 
