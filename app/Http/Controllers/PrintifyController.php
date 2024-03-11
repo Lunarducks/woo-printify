2d515358bcb19f0c7cb2873e8ce44e4e8ecf5733
root <root@ip-172-31-13-40.us-east-2.compute.internal>
2024-03-11 13:03:37 +0000

    latest

--- app/Http/Controllers/PrintifyController.php
+++ app/Http/Controllers/PrintifyController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\StoreProducts;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Config;
+use Illuminate\Support\Facades\Log;
 
 class PrintifyController extends Controller
 {
@@ -11,12 +14,12 @@ class PrintifyController extends Controller
 
     public function __construct()
     {
-        $this->printifyApiKey = env('PRINTIFY_API_KEY');
+        $this->printifyApiKey = config('printify.printify_token');
     }
 
     protected function printifyClient()
     {
-        return Http::baseUrl('https://api.printify.com/v1/')
+        return Http::baseUrl('https://api.printify.com/v1')
                    ->withHeaders([
                        'Authorization' => 'Bearer ' . $this->printifyApiKey,
                    ]);
@@ -44,7 +47,7 @@ class PrintifyController extends Controller
 
         $response = Http::withHeaders([
             'Authorization' => 'Bearer ' . $this->printifyApiKey,
-        ])->get("https://api.printify.com/v1/shops/{$shopId}/products.json");
+        ])->get("https://api.printify.com/v1/shops/{$shopId}/products.json?limit=2");
 
         if (!$response->successful()) {
             return response()->json(['error' => 'Failed to retrieve products'], $response->status());
@@ -52,8 +55,11 @@ class PrintifyController extends Controller
 
         $products = $response->json();
         $wooCommerceController = new WooCommerceController();
-        foreach ($products['data'] as $product) {
+        foreach ($products['data'] as $index => $product) {
             // Convert the product to a JSON string
+
+            ds($product);
+
             try {
                 // Import the product into WooCommerce
                 $wooCommerceController->importProductFromJson($product);
@@ -68,5 +74,43 @@ class PrintifyController extends Controller
         return response()->json(['success' => 'Products imported successfully.']);
     }
 
+    public function newImportProducts()
+    {
+
+       
+        $shopId = $this->getShopId(); 
+        if (!$shopId) {
+            $this->error('No shop ID found');
+            return;
+        }
+
+        $limit = 20; 
+       
+        $currentPage = cache()->get('printify_current_page', 1);
+       
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $this->printifyApiKey,
+        ])->get("https://api.printify.com/v1/shops/{$shopId}/products.json?limit={$limit}&page={$currentPage}");
+  
+        if (!$response->successful()) {
+            Log::error("https://api.printify.com/v1/shops/{$shopId}/products.json?limit={$limit}&page={$currentPage}");
+            return response()->json(['error' => 'Failed to retrieve products'], $response->status());
+        
+        }
+
+        $products = $response->json();
+ 
+        dispatch(new StoreProducts($products["data"]));
+        Log::info("https://api.printify.com/v1/shops/{$shopId}/products.json?limit={$limit}&page={$currentPage}");
     
+        $currentPage++;
+
+        if ($currentPage > $response->json()['last_page']) {
+            $currentPage = 1;
+        }
+
+        cache()->put('printify_current_page', $currentPage, 60 * 24); 
+
+        return response()->json(['success' => 'Products imported successfully.']);
+    }
 }
